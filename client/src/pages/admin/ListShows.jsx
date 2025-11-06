8d213046847e0ab86a79609e04420566f68d2f9d
Vaibhaveegamit-20 <gamit.v@northeastern.edu>
2025-11-06 16:49:32 -0500

    Adding client updates

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -3,35 +3,53 @@ import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import { dateFormat } from '../../lib/dateFormat';
+import { useAppContext } from '../../context/AppContext';
 
 const ListShows = () => {
 
   const currency = import.meta.env.VITE_CURRENCY
 
+  const {axios, getToken, user} = useAppContext()
+
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const getAllShows = async() => {
+  /*const getAllShows = async() => {
     try{
-      setShows([{
-        movie: dummyShowsData[0],
-        showDateTime: "2025-06-30T02:30:00.000Z",
-        showPrice: 59,
-        occupiedSeats: {
-          A1: "user_1",
-          B1: "user_2", 
-          C1: "user_3"
-        }
-      }]);
+      const {data} = await axios.get('/api/admin/all-shows', {headers: {Authorization: `Bearer ${await getToken()}` }});
+      setShows(data.shows)
       setLoading(false);
     }catch(error){
       console.error(error);
     }
+  } */
+
+    const getAllShows = async () => {
+  try {
+    const { data } = await axios.get('/api/admin/all-shows', {
+      headers: { Authorization: `Bearer ${await getToken()}` },
+    });
+
+    if (data?.success && Array.isArray(data.shows)) {
+      setShows(data.shows);
+    } else {
+      setShows([]);
+    }
+  } catch (error) {
+    console.error("Error fetching shows:", error);
+    setShows([]); // prevent map crash
+  } finally {
+    setLoading(false);
   }
+}; 
+
 
   useEffect(() => {
-    getAllShows();
-  }, []);
+    if(user){
+      getAllShows();
+    }
+    
+  }, [user]);
 
   return !loading ? (
     <>
